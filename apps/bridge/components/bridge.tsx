84c224107729f814a637b597e23c92eaf93731f7
Alex Harley <alex@alexbh.dev>
2024-02-15 22:48:52 +1300

    refactor: move banner to own component

--- apps/bridge/components/bridge.tsx
+++ apps/bridge/components/bridge.tsx
@@ -9,6 +9,7 @@ import { useWithdrawalsPaused } from "@/hooks/use-withdrawals-paused";
 import { PageTransition } from "./PageTransition";
 import { BridgeBody } from "./bridge-body";
 import { BridgeHeader } from "./bridge-header";
+import { WithdrawalsPaused } from "./withdrawals-paused";
 
 export const Bridge = () => {
   const deployment = useConfigState.useDeployment();
@@ -23,50 +24,7 @@ export const Bridge = () => {
       >
         <div className="w-full px-2 md:px-0  md:w-[420px] aspect-[3/4] relative mb-24 mt-16 md:mt-24 xl:mt-32">
           <div className="flex flex-col gap-2 md:gap-2 items-center">
-            {withdrawalsPaused && (
-              <div
-                className={`${
-                  deploymentTheme(deployment).bg
-                } flex items-center gap-3 w-full p-4 rounded-[16px]`}
-              >
-                <span>
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    width="60"
-                    height="60"
-                    fill="none"
-                    viewBox="0 0 60 60"
-                    className="h-8 w-8"
-                  >
-                    <g clipPath="url(#clip0_188_489)">
-                      <path
-                        fill="#FF3801"
-                        d="M31.28 51.8c0 4.45 3.15 7.84 7.51 7.84 4.36 0 7.51-3.42 7.51-7.84s-3.12-7.75-7.51-7.75-7.51 3.33-7.51 7.75zm-.54-44.53c0-4.3 3.75-7.27 8.11-7.27 4.36 0 8.14 3.06 8.14 7.27 0 1.03-.15 2.3-.33 3.94L43.87 36.4c-.39 3.66-2.03 5.66-5.03 5.66-3 0-4.63-2.24-5.03-5.66l-2.79-25.19c-.18-1.67-.3-2.91-.3-3.94h.02zM12 7.27C12 2.97 15.75 0 20.11 0c4.36 0 8.14 3.06 8.14 7.27 0 1.03-.15 2.3-.33 3.94L25.13 36.4c-.39 3.66-2.03 5.66-5.03 5.66-3 0-4.63-2.24-5.03-5.66L12.3 11.2c-.18-1.67-.3-2.91-.3-3.94v.01zm8.05 36.78c-4.3 0-7.51 3.33-7.51 7.75s3.15 7.84 7.51 7.84c4.36 0 7.51-3.42 7.51-7.84s-3.12-7.75-7.51-7.75z"
-                      ></path>
-                    </g>
-                    <defs>
-                      <clipPath id="clip0_188_489">
-                        <path
-                          fill="#fff"
-                          d="M0 0H35V59.64H0z"
-                          transform="translate(12)"
-                        ></path>
-                      </clipPath>
-                    </defs>
-                  </svg>
-                </span>
-                <span className="font-medium text-zinc-900 dark:text-white text-xs">
-                  OP Mainnet withdrawals are currently paused. <br />
-                  <Link
-                    href="https://status.optimism.io/"
-                    target="_blank"
-                    className="font-medium text-zinc-500 cursor-pointer hover:underline"
-                  >
-                    More info &amp; updates&hellip;
-                  </Link>
-                </span>
-              </div>
-            )}
+            {withdrawalsPaused && <WithdrawalsPaused />}
 
             <div
               className={`${
