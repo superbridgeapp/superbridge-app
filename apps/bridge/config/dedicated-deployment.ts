0a6804c80cf3e16788c2e599e19e450a5b585dfe
Alex Harley <alex@alexbh.dev>
2024-02-29 21:42:03 +1300

    feat: dedicated base deployment

--- apps/bridge/config/dedicated-deployment.ts
+++ apps/bridge/config/dedicated-deployment.ts
@@ -34,6 +34,14 @@ const mapping: { [name: string]: DedicatedDeployment | undefined } = {
       },
     ],
   },
+  base: {
+    name: "base",
+    footerLink: "https://base.org",
+    og: {
+      title: "Base Bridge",
+      description: "Bridge ETH and ERC20 tokens into and out of Base",
+    },
+  },
 };
 
 export const dedicatedDeployment = mapping[DEDICATED_DEPLOYMENT ?? ""] ?? null;
