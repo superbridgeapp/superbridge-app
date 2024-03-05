5c9de26e2b681730c467c1d366858c1467acad0a
Alex Harley <alex@alexbh.dev>
2024-03-06 09:07:25 +1300

    refactor: move over to new deployment logic

--- apps/bridge/hooks/use-deployments.ts
+++ apps/bridge/hooks/use-deployments.ts
@@ -1,8 +1,7 @@
-import { useMemo } from "react";
-
 import { useBridgeControllerGetDeployments } from "@/codegen";
 import {
   BridgeControllerGetDeploymentsParams,
+  DeploymentDto,
   DeploymentType,
 } from "@/codegen/model";
 import { dedicatedDeployment } from "@/config/dedicated-deployment";
@@ -77,88 +76,8 @@ export const useDeployments = () => {
     useDeploymentsFilters()
   );
 
-  const data = useMemo(() => {
-    return (
-      deployments.data?.data.filter((d) => {
-        if (DISABLED_DEPLOYMENTS.includes(d.name)) {
-          return false;
-        }
-
-        if (dedicatedDeployment) {
-          return d.name === dedicatedDeployment.name;
-        }
-
-        if (window.location.hostname === "localhost") {
-          return true;
-        }
-
-        if (window.location.hostname.includes("vercel.app")) {
-          return true;
-        }
-
-        // superchain mainnets
-        if (window.location.hostname === "superbridge.app") {
-          return [
-            "optimism",
-            "base",
-            "zora",
-            "pgn",
-            "mode",
-            "orderly",
-            "lyra",
-          ].includes(d.name);
-        }
-
-        // superchain testnets
-        if (window.location.hostname === "testnets.superbridge.app") {
-          return [
-            "op-sepolia",
-            "base-sepolia",
-            "zora-sepolia-0thyhxtf5e",
-            "pgn-sepolia-i4td3ji6i0",
-            "mode-sepolia-vtnhnpim72",
-            "orderly-l2-4460-sepolia-8tc3sd7dvy",
-          ].includes(d.name);
-        }
-
-        // main site shows all mainnet deployments
-        if (window.location.hostname === "app.rollbridge.app") {
-          return d.type === DeploymentType.mainnet;
-        }
-
-        // these need to go last so they don't clash with devnets. or testnets. subdomains
-
-        // [id].devnets.superbridge|rollbridge.app
-        if (
-          window.location.hostname.includes("devnets.superbridge.app") ||
-          window.location.hostname.includes("devnets.rollbridge.app")
-        ) {
-          const [id] = window.location.hostname.split(".");
-          return (
-            d.type === DeploymentType.devnet &&
-            (d.conduitId === id || d.name === id)
-          );
-        }
-
-        // [id].testnets.superbridge|rollbridge.app
-        if (
-          window.location.hostname.includes("testnets.superbridge.app") ||
-          window.location.hostname.includes("testnets.rollbridge.app")
-        ) {
-          const [id] = window.location.hostname.split(".");
-          return (
-            d.type === DeploymentType.testnet &&
-            (d.conduitId === id || d.name === id)
-          );
-        }
-
-        return false;
-      }) ?? []
-    );
-  }, [deployments.data?.data]);
-
   return {
-    deployments: data,
+    deployments: deployments.data?.data ?? ([] as DeploymentDto[]),
     isLoading: deployments.isLoading,
     isError: deployments.isError,
   };
