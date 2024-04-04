8d9317a23560adbf85d7775dc954cf85a726abc0
James Stackhouse <james@houseofstack.com>
2024-04-04 17:09:31 +1100

    button tweak

--- apps/bridge/components/ui/button.tsx
+++ apps/bridge/components/ui/button.tsx
@@ -20,9 +20,9 @@ const buttonVariants = cva(
         link: "text-primary underline-offset-4 hover:underline",
       },
       size: {
-        default: "h-10 px-4 py-6",
-        sm: "h-9 rounded-md px-3",
-        lg: "h-11 rounded-md px-8",
+        default: "h-12 px-4 py-2",
+        sm: "h-9 rounded-full px-3",
+        lg: "h-11 rounded-full px-8",
         icon: "h-10 w-10",
       },
     },
