c52fbba688355283bcd9ba0a4a75d576ad5105e4
Alex Harley <alex@alexbh.dev>
2024-04-09 15:32:29 +1200

    experimentation

--- apps/bridge/state/theme.ts
+++ apps/bridge/state/theme.ts
@@ -2,6 +2,8 @@ import { createSelectorHooks } from "auto-zustand-selectors-hook";
 import { create } from "zustand";
 
 interface ThemeState {
+  darkModeEnabled: boolean | null;
+  setDarkModeEnabled: (d: boolean) => void;
   navIcon: string | null;
   setNavIcon: (d: string) => void;
 }
@@ -9,6 +11,9 @@ interface ThemeState {
 const themeState = create<ThemeState>()((set) => ({
   navIcon: null,
   setNavIcon: (navIcon) => set({ navIcon }),
+
+  darkModeEnabled: null,
+  setDarkModeEnabled: (darkModeEnabled) => set({ darkModeEnabled }),
 }));
 
 export const useThemeState = createSelectorHooks(themeState);
