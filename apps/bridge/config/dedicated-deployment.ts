fe5e6935605675cdb5ca2b27bc8031b4dc915a85
Alex Harley <alex@alexbh.dev>
2024-04-19 09:23:18 +1200

    feat: ebi-mainnet

--- apps/bridge/config/dedicated-deployment.ts
+++ apps/bridge/config/dedicated-deployment.ts
@@ -120,6 +120,14 @@ export const dedicatedDeploymentMapping: {
     },
     network: "Ebi Testnet",
   },
+  ["ebi-mainnet"]: {
+    name: "ebi-mainnet",
+    og: {
+      title: "Ebi Mainnet Bridge",
+      description: "Bridge ETH and ERC20 tokens into and out of Ebi Mainnet",
+    },
+    network: "Ebi Mainnet",
+  },
   ["mint-sepolia-testnet-ijtsrc4ffq"]: {
     name: "mint-sepolia-testnet-ijtsrc4ffq",
     og: {
