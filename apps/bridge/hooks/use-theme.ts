bd45d0362dfad0b4efc4e3a37e03e2429019f8a0
Alex Harley <alex@alexbh.dev>
2024-04-20 08:59:55 +1200

    feat: defaults

--- apps/bridge/hooks/use-theme.ts
+++ apps/bridge/hooks/use-theme.ts
@@ -7,56 +7,62 @@ import { useTheme } from "next-themes";
 
 export const useNavIcon = () => {
   const { deployments } = useDeployments();
-
   const theme = useContext(ThemeContext);
+  const { resolvedTheme } = useTheme();
 
-  if (theme?.imageLogo) return theme.imageLogo;
-  if (deployments.length === 1) return deployments[0].theme?.theme.imageLogo;
+  const defaultIcon = "/img/rollbridge-logo.svg";
+
   if (isSuperbridge) return "/img/logo.svg";
-  return "/img/rollbridge-logo.svg";
+
+  if (resolvedTheme === "light") {
+    return (
+      theme?.imageLogo ?? deployments[0]?.theme?.theme.imageLogo ?? defaultIcon
+    );
+  }
+
+  return (
+    theme?.imageLogoDark ??
+    deployments[0]?.theme?.theme.imageLogoDark ??
+    defaultIcon
+  );
 };
 
 export const useNetworkIcon = () => {
   const { deployments } = useDeployments();
-
   const theme = useContext(ThemeContext);
-  if (theme?.imageNetwork) return theme.imageNetwork;
-  if (deployments.length === 1) return deployments[0].theme?.theme.imageNetwork;
+
+  const defaultIcon = "/img/L2.svg";
+
   if (isSuperbridge) return "/img/logo.svg";
-  return "/img/rollbridge-logo.svg";
+
+  return (
+    theme?.imageNetwork ??
+    deployments[0].theme?.theme.imageNetwork ??
+    defaultIcon
+  );
 };
 
 export const useBackgroundIcon = () => {
   const { deployments } = useDeployments();
-
   const theme = useContext(ThemeContext);
-  if (theme?.imageBackground) return theme.imageBackground;
-  if (deployments.length === 1)
-    return deployments[0].theme?.theme.imageBackground;
-  return null;
+  const { resolvedTheme } = useTheme();
+
+  const defaultIcon = null;
+
+  if (resolvedTheme === "light") {
+    return (
+      theme?.imageBackground ??
+      deployments[0]?.theme?.theme.imageBackground ??
+      defaultIcon
+    );
+  }
+
+  return (
+    theme?.imageBackgroundDark ??
+    deployments[0]?.theme?.theme.imageBackgroundDark ??
+    defaultIcon
+  );
 };
-
-// export const useNavIconDark = () => {
-//   const { deployments } = useDeployments();
-
-//   if (deployments.length === 1) return deployments[0].theme?.theme.navIconSrc;
-//   if (isSuperbridge) return "/img/logo-dark.svg";
-//   return "/img/rollbridge-logo-dark.svg";
-// };
-// export const useNavIconSmall = () => {
-//   const { deployments } = useDeployments();
-
-//   if (deployments.length === 1) return null;
-//   if (isSuperbridge) return "/img/logo-small.svg";
-//   return "/img/rollbridge-logo-small.svg";
-// };
-// export const useNavIconSmallDark = () => {
-//   const { deployments } = useDeployments();
-
-//   if (deployments.length === 1) return null;
-//   if (isSuperbridge) return "/img/logo-small-dark.svg";
-//   return "/img/rollbridge-logo-small-dark.svg";
-// };
 
 export const useDarkModeEnabled = () => {
   const { deployments } = useDeployments();
