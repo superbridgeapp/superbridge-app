f89282435f52fe1bd4a654143f03104f301fac52
Alex Harley <alex@alexbh.dev>
2024-05-14 15:25:47 +0200

    feat: client privacy & terms

---
+++ apps/bridge/pages/client-terms.tsx
@@ -0,0 +1,43 @@
+import { GetServerSidePropsContext, InferGetServerSidePropsType } from "next";
+import ReactMarkdown from "react-markdown";
+
+import PageFooter from "@/components/page-footer";
+import PageNav from "@/components/page-nav";
+
+import {
+  bridgeControllerGetDeployments,
+  bridgeControllerGetDeploymentsByDomain,
+} from "@/codegen/index";
+
+export default function ClientTerms({
+  deployment,
+}: InferGetServerSidePropsType<typeof getServerSideProps>) {
+  return (
+    <div className="w-screen h-screen overflow-y-auto bg-zinc-50">
+      <PageNav />
+      <main>
+        <section className="max-w-3xl mx-auto p-8 prose prose-sm prose-headings:font-bold dark:prose-invert">
+          <ReactMarkdown>{deployment?.tos?.customTermsOfService}</ReactMarkdown>
+        </section>
+      </main>
+      <PageFooter />
+    </div>
+  );
+}
+
+export const getServerSideProps = async ({
+  req,
+}: GetServerSidePropsContext) => {
+  if (!req.headers.host) {
+    return { props: { deployment: null } };
+  }
+
+  const { data } = await bridgeControllerGetDeploymentsByDomain(
+    req.headers.host
+  );
+  // const { data } = await bridgeControllerGetDeployments({
+  //   names: ["arbitrum-one"],
+  // });
+
+  return { props: { deployment: data[0] ?? null } };
+};
