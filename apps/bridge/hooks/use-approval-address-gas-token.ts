81b54d31cccc7496649f864a5887cc21bdb01d76
Alex Harley <alex@alexbh.dev>
2024-05-24 14:33:37 +0200

    fix: bridging ERC20s on Orbit chains configured with a native token

---
+++ apps/bridge/hooks/use-approval-address-gas-token.ts
@@ -0,0 +1,30 @@
+import { Address } from "viem";
+
+import { isArbitrum, isOptimism } from "@/utils/is-mainnet";
+import { useConfigState } from "@/state/config";
+import { isArbitrumToken } from "@/utils/guards";
+
+import { useFromChain, useToChain } from "./use-chain";
+import { useDeployment } from "./use-deployment";
+
+export function useApprovalAddressGasToken(): Address | undefined {
+  const from = useFromChain();
+  const to = useToChain();
+  const deployment = useDeployment();
+  const stateToken = useConfigState.useToken();
+
+  const token = stateToken?.[from?.id ?? 0];
+
+  if (!deployment) return;
+
+  if (isArbitrum(deployment)) {
+    if (token && isArbitrumToken(token)) {
+      return token.arbitrumBridgeInfo[to?.id ?? 0];
+    }
+    return deployment.contractAddresses.inbox as Address;
+  }
+
+  if (isOptimism(deployment)) {
+    return deployment.contractAddresses.optimismPortal as Address;
+  }
+}
