528296b7da7587f55a2cc2f3f294d5fe8de07d20
Alex Harley <alex@alexbh.dev>
2024-05-30 16:24:43 +0200

    feat: API key header

--- apps/bridge/utils/query-client.ts
+++ apps/bridge/utils/query-client.ts
@@ -4,6 +4,7 @@ import Axios from "axios";
 export const baseURL = process.env["NEXT_PUBLIC_API_URL"];
 
 Axios.defaults.baseURL = baseURL;
+Axios.defaults.headers["x-api-key"] = process.env["SUPERBRIDGE_API_KEY"]!;
 
 export const queryClient = new QueryClient({
   defaultOptions: {
