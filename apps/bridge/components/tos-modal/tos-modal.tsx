e622defb4a92a71b7b1fea8b6acee40ceedbc9c9
Alex Harley <alex@alexbh.dev>
2024-06-05 09:09:27 +0200

    refactor

--- apps/bridge/components/tos-modal/tos-modal.tsx
+++ apps/bridge/components/tos-modal/tos-modal.tsx
@@ -136,7 +136,13 @@ export const TosModal = () => {
     </div>
   );
 
-  const Terms = () => {
+  const ForceReadScroll = ({
+    title,
+    content,
+  }: {
+    title: string;
+    content: string;
+  }) => {
     const scrollRef = useRef<HTMLDivElement>(null);
     const [scrolled, setScrolled] = useState(false);
 
@@ -156,11 +162,6 @@ export const TosModal = () => {
       restDelta: 0.001,
     });
 
-    const onClick = () => {
-      if (!scrolled) return;
-      onNext();
-    };
-
     return (
       <div className="flex flex-col">
         <div className="flex items-start justify-start bg-muted h-1">
@@ -173,66 +174,12 @@ export const TosModal = () => {
           ref={scrollRef}
           className="max-h-[320px] prose prose-sm prose-headings:font-bold dark:prose-invert overflow-y-scroll p-6"
         >
-          <h1 className="text-lg font-bold text-foreground">
-            {t("tos.forceReadTerms")}
-          </h1>
-          <ReactMarkdown>{deployment?.tos?.customTermsOfService}</ReactMarkdown>
+          <h1 className="text-lg font-bold text-foreground">{title}</h1>
+          <ReactMarkdown>{content}</ReactMarkdown>
         </div>
         <div className="border-t border-muted p-6 relative">
           {!scrolled && <ScrollArrow />}
-          <Button disabled={!scrolled} className="w-full" onClick={onClick}>
-            {t("tos.agreeAndContinue")}
-          </Button>
-        </div>
-      </div>
-    );
-  };
-  const Privacy = () => {
-    const scrollRef = useRef<HTMLDivElement>(null);
-    const [scrolled, setScrolled] = useState(false);
-
-    const { scrollYProgress } = useScroll({
-      container: scrollRef,
-    });
-
-    useMotionValueEvent(scrollYProgress, "change", (latest: any) => {
-      console.log("Page scroll: ", latest);
-      if (latest >= 0.8) {
-        setScrolled(true);
-      }
-    });
-
-    const scaleX = useSpring(scrollYProgress, {
-      stiffness: 100,
-      damping: 30,
-      restDelta: 0.001,
-    });
-
-    const onClick = () => {
-      if (!scrolled) return;
-      onNext();
-    };
-
-    return (
-      <div className="flex flex-col">
-        <div className="flex items-start justify-start bg-muted h-1">
-          <motion.div
-            className="w-full bg-muted-foreground h-1 origin-top-left"
-            style={{ scaleX }}
-          />
-        </div>
-        <div
-          ref={scrollRef}
-          className="max-h-[320px] prose prose-sm prose-headings:font-bold dark:prose-invert overflow-y-scroll p-6"
-        >
-          <h1 className="text-lg font-bold text-foreground">
-            {t("tos.forceReadPrivacy")}
-          </h1>
-          <ReactMarkdown>{deployment?.tos?.customPrivacyPolicy}</ReactMarkdown>
-        </div>
-        <div className="border-t border-muted p-6 relative">
-          {!scrolled && <ScrollArrow />}
-          <Button disabled={!scrolled} className="w-full" onClick={onClick}>
+          <Button disabled={!scrolled} className="w-full" onClick={onNext}>
             {t("tos.agreeAndContinue")}
           </Button>
         </div>
@@ -242,10 +189,26 @@ export const TosModal = () => {
 
   const tabs = [{ name: "tab1", component: tab1 }];
   if (deployment?.tos?.forceReadTermsOfService) {
-    tabs.push({ name: "terms", component: <Terms /> });
+    tabs.push({
+      name: "terms",
+      component: (
+        <ForceReadScroll
+          title={t("tos.forceReadTerms")}
+          content={deployment?.tos?.customTermsOfService ?? ""}
+        />
+      ),
+    });
   }
   if (deployment?.tos?.forceReadPrivacyPolicy) {
-    tabs.push({ name: "privacy", component: <Privacy /> });
+    tabs.push({
+      name: "privacy",
+      component: (
+        <ForceReadScroll
+          title={t("tos.forceReadPrivacy")}
+          content={deployment?.tos?.customPrivacyPolicy ?? ""}
+        />
+      ),
+    });
   }
 
   return (
