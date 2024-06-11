299b530459a6776b8d32a922412ae93ff4b832af
Alex Harley <alex@alexbh.dev>
2024-06-11 03:07:30 +0200

    chore: hide fault proof banner

--- apps/bridge/hooks/use-fault-proof-upgrade-time.ts
+++ apps/bridge/hooks/use-fault-proof-upgrade-time.ts
@@ -1,7 +1,8 @@
 import { DeploymentDto } from "@/codegen/model";
 
 export const useFaultProofUpgradeTime = (deployment: DeploymentDto | null) => {
-  return deployment?.name === "optimism"
-    ? new Date("6/10/2024").getTime()
-    : null;
+  return null;
+  // deployment?.name === "optimism"
+  //   ? new Date("6/10/2024").getTime()
+  //   : null;
 };
