5f997a6f28b0ee3ad22816583d76e5999eb6adbc
Alex Harley <alex@alexbh.dev>
2024-06-25 14:22:47 +0200

    chore: clean up

--- apps/bridge/components/Layout.tsx
+++ apps/bridge/components/Layout.tsx
@@ -39,7 +39,6 @@ export function Layout({ children }: { children: any }) {
   const displayTransactions = useConfigState.useDisplayTransactions();
   const setSettingsModal = useConfigState.useSetSettingsModal();
   const settingsModal = useConfigState.useSettingsModal();
-  const setFast = useConfigState.useSetFast();
   const fast = useConfigState.useFast();
   const pathname = usePathname();
 
@@ -228,49 +227,12 @@ export function Layout({ children }: { children: any }) {
           />
         </div>
       </nav>
-      {/* controls */}
-      {/* {isSuperbridge && !displayTransactions && (
-        <>
-          <motion.button
-            onClick={() => navigate("/")}
-            whileHover={{ scale: 1.05 }}
-            animate={{ y: pathname === "/" && !fast ? -80 : 0, x: "-50%" }}
-            initial={{ y: -60, x: "-50%" }}
-            className={`flex items-center py-0 md:py-3 px-3 md:px-4 h-10 md:h-auto absolute bg-card top-3 md:top-6  md:left-1/2 z-10 rounded-full shadow-sm left-[102px]`}
-          >
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              width="27"
-              height="16"
-              className="fill-foreground"
-              viewBox="0 0 27 16"
-            >
-              <g clipPath="url(#clip0_364_6103)">
-                <path d="M0 3.389C0 1.402 1.413 0 3.46 0h5.352c2.058 0 3.46 1.402 3.46 3.389 0 1.987-1.402 3.405-3.46 3.405H3.46C1.413 6.794 0 5.396 0 3.389zm1.32.191c.234 0 .382-.164.425-.425.098-.781.578-1.304 1.375-1.414.234-.043.41-.218.398-.425 0-.219-.192-.398-.453-.398-.055 0-.152 0-.219.015-1.003.18-1.741.976-1.909 2.003-.016.082-.016.152-.016.219 0 .289.235.425.399.425zM14.15 3.389C14.15 1.398 15.562 0 17.608 0h5.352c2.058 0 3.46 1.402 3.46 3.389 0 1.987-1.402 3.405-3.46 3.405H17.61c-2.046 0-3.46-1.402-3.46-3.405zm1.32.191c.233 0 .382-.164.425-.425.097-.781.578-1.304 1.374-1.414.234-.043.41-.218.398-.425 0-.219-.191-.398-.453-.398-.054 0-.152 0-.218.015-1.004.18-1.742.976-1.91 2.003-.015.082-.015.152-.015.219 0 .289.234.425.398.425zM0 12.595c0-1.99 1.413-3.388 3.46-3.388h5.352c2.058 0 3.46 1.401 3.46 3.388 0 1.988-1.402 3.405-3.46 3.405H3.46C1.413 16 0 14.598 0 12.595zm1.32.192c.234 0 .382-.164.425-.426.098-.78.578-1.304 1.375-1.413.234-.043.41-.219.398-.426 0-.218-.192-.398-.453-.398-.055 0-.152 0-.219.016-1.003.18-1.741.976-1.909 2.003-.016.082-.016.152-.016.218 0 .29.235.426.399.426zM14.15 12.595c0-1.99 1.413-3.388 3.459-3.388h5.352c2.058 0 3.46 1.401 3.46 3.388 0 1.988-1.402 3.405-3.46 3.405H17.61c-2.046 0-3.46-1.402-3.46-3.405zm1.32.192c.233 0 .382-.164.425-.426.097-.78.578-1.304 1.374-1.413.234-.043.41-.219.398-.426 0-.218-.191-.398-.453-.398-.054 0-.152 0-.218.016-1.004.18-1.742.976-1.91 2.003-.015.082-.015.152-.015.218 0 .29.234.426.398.426z"></path>
-              </g>
-              <defs>
-                <clipPath id="clip0_364_6103">
-                  <path fill="#fff" d="M0 0H26.421V16H0z"></path>
-                </clipPath>
-              </defs>
-            </svg>
-          </motion.button>
 
-          <motion.button
-            onClick={() => navigate("fast")}
-            whileHover={{ scale: 1.05 }}
-            animate={{ y: 0, x: "20%" }}
-            initial={{ y: -60, x: "20%" }}
-            className={`flex items-center py-0 md:py-3 px-3 md:px-4 h-10 md:h-auto absolute bg-card top-3 md:top-6  md:left-1/2 z-10 rounded-full shadow-sm left-[90px]`}
-          >
-            Fast
-          </motion.button>
-        </>
-      )} */}
-
+      {/* bridge */}
       <AnimatePresence mode="sync" initial={false}>
         {displayTransactions ? null : children}
       </AnimatePresence>
+
       {/* Transactions container */}
       <AnimatePresence mode="sync" initial={false}>
         {displayTransactions ? (
