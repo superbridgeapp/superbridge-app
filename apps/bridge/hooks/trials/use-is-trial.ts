4bbef8835d1a68b3c14f62b2b40eebc4ba8ca5aa
Alex <alex@alexbh.dev>
2024-06-29 19:41:34 +0200

    Rollie upgrade promo (#78)
    Co-authored-by: James Stackhouse <james@houseofstack.com>

--- apps/bridge/hooks/trials/use-is-trial.ts
+++ apps/bridge/hooks/trials/use-is-trial.ts
@@ -1,15 +1,14 @@
-// import { isSuperbridge } from "@/config/superbridge";
-// import { isTrial } from "@/utils/guards";
+import { isSuperbridge } from "@/config/superbridge";
+import { isTrial } from "@/utils/guards";
 
-// import { useDeployment } from "../use-deployment";
+import { useDeployment } from "../use-deployment";
 
 export const useIsTrial = () => {
-  return false;
-  // const deployment = useDeployment();
+  const deployment = useDeployment();
 
-  // if (isSuperbridge || !deployment) {
-  //   return false;
-  // }
+  if (isSuperbridge || !deployment) {
+    return false;
+  }
 
-  // return isTrial(deployment.status);
+  return isTrial(deployment.status);
 };
