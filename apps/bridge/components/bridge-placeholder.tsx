d996f7d262af8d685597244da749887fd4e8b38a
James Stackhouse <james@houseofstack.com>
2024-07-09 14:24:40 +1000

    New bridge badge

--- apps/bridge/components/bridge-placeholder.tsx
+++ apps/bridge/components/bridge-placeholder.tsx
@@ -4,6 +4,7 @@ import { useTranslation } from "react-i18next";
 
 import { DeploymentDto, DeploymentType } from "@/codegen/model";
 import { deploymentTheme } from "@/config/theme";
+import { BadgeNew } from "./badges";
 
 export const BridgePlaceholder = ({
   deployment,
@@ -17,57 +18,65 @@ export const BridgePlaceholder = ({
   const theme = deploymentTheme(deployment);
   const { t } = useTranslation();
   return (
-    <div
-      className={`relative w-full h-full flex flex-col shrink-0 cursor-pointer overflow-hidden rounded-[21px] lg:rounded-[32px] shadow-sm ${theme.card.className}`}
-    >
-      {theme.card.overlay?.image ? (
-        <Image
-          src={theme.card.overlay.image}
-          className={clsx("inset-0 z-0 absolute", theme.card.overlay.className)}
-          alt={deployment.displayName}
-          fill
-          loading="eager"
-          sizes="(max-width: 767px) 50vw, (max-width: 1023px) 33vw, 25vw"
-        />
-      ) : (
-        <div
-          className={clsx(
-            "inset-0 z-0 absolute",
-            theme.card.overlay?.className
-          )}
-        />
-      )}
-      <div className="flex gap-4 flex-col capitalize items-center justify-center px-3 md:px-6 grow w-full relative z-10">
-        <Image
-          src={theme.iconSrc}
-          width={96}
-          height={96}
-          alt="Chain"
-          className={`pointer-events-none w-16 h-16 md:w-24 md:h-24 ${
-            comingSoon ? "opacity-100" : "opacity-100"
-          }`}
-        />
-        <h2
-          className={clsx(
-            "text-sm text-center font-heading",
-            comingSoon ? "opacity-50" : "opacity-100",
-            theme.card.title ?? "text-zinc-50"
-          )}
-        >
-          {deployment.displayName}
-        </h2>
+    <>
+      <div
+        className={`relative w-full h-full flex flex-col shrink-0 cursor-pointer overflow-hidden rounded-[21px] lg:rounded-[32px] shadow-sm ${theme.card.className}`}
+      >
+        {theme.card.overlay?.image ? (
+          <Image
+            src={theme.card.overlay.image}
+            className={clsx(
+              "inset-0 z-0 absolute",
+              theme.card.overlay.className
+            )}
+            alt={deployment.displayName}
+            fill
+            loading="eager"
+            sizes="(max-width: 767px) 50vw, (max-width: 1023px) 33vw, 25vw"
+          />
+        ) : (
+          <div
+            className={clsx(
+              "inset-0 z-0 absolute",
+              theme.card.overlay?.className
+            )}
+          />
+        )}
+        <div className="flex gap-4 flex-col capitalize items-center justify-center px-3 md:px-6 grow w-full relative z-10">
+          <Image
+            src={theme.iconSrc}
+            width={96}
+            height={96}
+            alt="Chain"
+            className={`pointer-events-none w-16 h-16 md:w-24 md:h-24 ${
+              comingSoon ? "opacity-100" : "opacity-100"
+            }`}
+          />
+          <h2
+            className={clsx(
+              "text-sm text-center font-heading",
+              comingSoon ? "opacity-50" : "opacity-100",
+              theme.card.title ?? "text-zinc-50"
+            )}
+          >
+            {deployment.displayName}
+          </h2>
+        </div>
+        {/* coming soon */}
+        {comingSoon && (
+          <span className="text-[10px]  inline-flex leading-none bg-black/30 text-white  absolute bottom-6 left-1/2 -translate-x-1/2 rounded-full px-2 py-1">
+            {t("comingSoon")}
+          </span>
+        )}
+        {!comingSoon && deployment.type === DeploymentType.testnet && (
+          <span className="text-[10px]  inline-flex leading-none bg-black/30 text-white  absolute bottom-6 left-1/2 -translate-x-1/2 rounded-full px-2 py-1">
+            Testnet
+          </span>
+        )}
       </div>
-      {/* coming soon */}
-      {comingSoon && (
-        <span className="text-[10px]  inline-flex leading-none bg-black/30 text-white  absolute bottom-6 left-1/2 -translate-x-1/2 rounded-full px-2 py-1">
-          {t("comingSoon")}
-        </span>
+      {newDeployment && (
+        <BadgeNew className="absolute -top-1 -right-1.5 w-8 h-8" />
       )}
-      {!comingSoon && deployment.type === DeploymentType.testnet && (
-        <span className="text-[10px]  inline-flex leading-none bg-black/30 text-white  absolute bottom-6 left-1/2 -translate-x-1/2 rounded-full px-2 py-1">
-          Testnet
-        </span>
-      )}
-    </div>
+    </>
   );
 };
