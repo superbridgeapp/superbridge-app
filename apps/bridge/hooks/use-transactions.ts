69886d544b1e02cda98ccf2041ce1c75446aefc1
Alex Harley <alex@alexbh.dev>
2024-07-17 11:33:08 +0200

    refactor: activity effects

--- apps/bridge/hooks/use-transactions.ts
+++ apps/bridge/hooks/use-transactions.ts
@@ -1,18 +1,10 @@
 import { useInfiniteQuery } from "@tanstack/react-query";
-import { useEffect } from "react";
+import { useMemo } from "react";
 import { useAccount } from "wagmi";
 
 import { bridgeControllerGetActivityV3 } from "@/codegen";
 import { isSuperbridge } from "@/config/superbridge";
 import { useInjectedStore } from "@/state/injected";
-import { usePendingTransactions } from "@/state/pending-txs";
-import {
-  isForcedWithdrawal,
-  isOptimismForcedWithdrawal,
-  isOptimismWithdrawal,
-  isWithdrawal,
-} from "@/utils/guards";
-import { getInitiatingHash } from "@/utils/initiating-tx-hash";
 
 import { useDeployments } from "./use-deployments";
 
@@ -21,9 +13,6 @@ export const useTransactions = () => {
   const { deployments } = useDeployments();
 
   const superbridgeTestnetsEnabled = useInjectedStore((s) => s.testnets);
-  const removeFinalising = usePendingTransactions.useRemoveFinalising();
-  const removeProving = usePendingTransactions.useRemoveProving();
-  const removePending = usePendingTransactions.useRemoveTransactionByHash();
 
   const { data, isLoading, isFetchingNextPage, isError, fetchNextPage } =
     useInfiniteQuery({
@@ -57,31 +46,11 @@ export const useTransactions = () => {
       refetchInterval: 10_000,
     });
 
-  useEffect(() => {
-    if (!data?.pages) {
-      return;
-    }
-
-    data.pages.forEach(({ transactions }) =>
-      transactions.forEach((tx) => {
-        const hash = getInitiatingHash(tx);
-        if (hash) removePending(hash);
-
-        if (isWithdrawal(tx)) {
-          if (isOptimismWithdrawal(tx)) if (tx.prove) removeProving(tx.id);
-          if (tx.finalise) removeFinalising(tx.id);
-        }
-        if (isForcedWithdrawal(tx)) {
-          if (isOptimismForcedWithdrawal(tx))
-            if (tx.withdrawal?.prove) removeProving(tx.id);
-          if (tx.withdrawal?.finalise) removeFinalising(tx.id);
-        }
-      })
-    );
-  }, [data?.pages, removePending, removeProving, removeFinalising]);
-
   return {
-    transactions: data?.pages.flatMap((p) => p.transactions) ?? [],
+    transactions: useMemo(
+      () => data?.pages.flatMap((p) => p.transactions) ?? [],
+      [data?.pages]
+    ),
     isLoading,
     isFetchingNextPage,
     isError,
