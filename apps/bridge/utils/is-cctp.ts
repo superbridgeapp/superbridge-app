ebb37f8908fc9aa00c107d559f7ee66f2681092e
Alex Harley <alex@alexbh.dev>
2024-07-17 13:22:05 +0200

    chore: rm eurc

--- apps/bridge/utils/is-cctp.ts
+++ apps/bridge/utils/is-cctp.ts
@@ -1,19 +1,13 @@
 import { isAddressEqual } from "viem";
 
 import { MultiChainToken } from "@/types/token";
-import { eurc } from "@/utils/token-list/json/cctp/eurc";
 import * as usdc from "@/utils/token-list/json/cctp/usdc";
 
 // https://developers.circle.com/stablecoins/docs/usdc-on-main-networks
 export const isCctp = (token: MultiChainToken | null) => {
   if (!token) return false;
-  return (
-    usdc.native.every((x) =>
-      Object.values(token).find((t) => isAddressEqual(t.address, x.address))
-    ) ||
-    eurc.every((x) =>
-      Object.values(token).find((t) => isAddressEqual(t.address, x.address))
-    )
+  return usdc.native.every((x) =>
+    Object.values(token).find((t) => isAddressEqual(t.address, x.address))
   );
 };
 
