e89d4995215291f5d4f6f5db7d2d13cb1c75e143
Alex <alex@alexbh.dev>
2024-07-17 12:41:22 +0200

    Deleted bridges (#100)
    Co-authored-by: James Stackhouse <james@houseofstack.com>

--- apps/bridge/components/bridge.tsx
+++ apps/bridge/components/bridge.tsx
@@ -10,8 +10,10 @@ import { PoweredByConduit } from "./badges/powered-by-conduit-badge";
 import { TestnetBadge } from "./badges/testnet-badge";
 import { FaultProofsBanner } from "./banners/fault-proofs-banner";
 import { HasWithdrawalReadyToFinalizeBanner } from "./banners/has-withdrawal-ready-to-finalize-banner.ts";
+import { ScheduledDeletion } from "./banners/scheduled-deletion";
 import { WithdrawalsPaused } from "./banners/withdrawals-paused";
 import { BridgeBody } from "./bridge-body";
+import { BridgeDeleted } from "./bridge-deleted";
 import { BridgeHeader } from "./bridge-header";
 import { UpgradePromo } from "./upgrade-promo";
 
@@ -28,6 +30,10 @@ export const Bridge = () => {
     >
       <div className="w-full px-2 md:px-0  md:w-[420px] aspect-[3/4] relative mb-24 mt-28 md:mt-24 2xl:mt-32">
         <div className="flex flex-col gap-2 items-center">
+          {deployment?.deletedAt &&
+            new Date(deployment.deletedAt) > new Date() && (
+              <ScheduledDeletion />
+            )}
           {withdrawalsPaused && <WithdrawalsPaused />}
           {faultProofUpgradeTime && <FaultProofsBanner />}
           {hasWithdrawalReadyToFinalize && (
@@ -37,23 +43,30 @@ export const Bridge = () => {
           <div
             className={`bg-card mx-auto rounded-[24px] md:rounded-[32px] shadow-sm w-full shrink-0 backdrop-blur-sm`}
           >
-            <BridgeHeader />
-            <BridgeBody />
+            {deployment?.deletedAt &&
+            new Date(deployment.deletedAt) < new Date() ? (
+              <BridgeDeleted />
+            ) : (
+              <>
+                <BridgeHeader />
+                <BridgeBody />
 
-            {(deployment?.type === DeploymentType.testnet ||
-              (deployment?.provider === "conduit" && isSuperbridge) ||
-              (deployment?.provider === "alt-layer" && isSuperbridge)) && (
-              <div className="flex gap-2 py-1 justify-center items-center -translate-y-2">
-                {deployment?.type === DeploymentType.testnet && (
-                  <TestnetBadge />
+                {(deployment?.type === DeploymentType.testnet ||
+                  (deployment?.provider === "conduit" && isSuperbridge) ||
+                  (deployment?.provider === "alt-layer" && isSuperbridge)) && (
+                  <div className="flex gap-2 py-1 justify-center items-center -translate-y-2">
+                    {deployment?.type === DeploymentType.testnet && (
+                      <TestnetBadge />
+                    )}
+                    {deployment?.provider === "conduit" && isSuperbridge && (
+                      <PoweredByConduit />
+                    )}
+                    {deployment?.provider === "alt-layer" && isSuperbridge && (
+                      <PoweredByAltLayer />
+                    )}
+                  </div>
                 )}
-                {deployment?.provider === "conduit" && isSuperbridge && (
-                  <PoweredByConduit />
-                )}
-                {deployment?.provider === "alt-layer" && isSuperbridge && (
-                  <PoweredByAltLayer />
-                )}
-              </div>
+              </>
             )}
           </div>
 
