1095d8862c95a8ef3e4ea577810f537babbbeb97
Alex Harley <alex@alexbh.dev>
2024-07-23 19:01:40 +0200

    chore: disable Base Sepolia fault proofs modal

--- apps/bridge/hooks/use-fault-proof-upgrade-time.ts
+++ apps/bridge/hooks/use-fault-proof-upgrade-time.ts
@@ -1,7 +1,8 @@
 import { DeploymentDto } from "@/codegen/model";
 
 export const useFaultProofUpgradeTime = (deployment: DeploymentDto | null) => {
-  return deployment?.name === "base-sepolia"
-    ? new Date("2024/7/23").getTime()
-    : null;
+  return null;
+  // return deployment?.name === "base-sepolia"
+  //   ? new Date("2024/7/23").getTime()
+  //   : null;
 };
