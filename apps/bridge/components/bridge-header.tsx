924ec3433165f7c80a6b063c6966203b18ce6ac4
Alex Harley <alex@alexbh.dev>
2024-07-28 11:49:40 +0200

    in progress count

--- apps/bridge/components/bridge-header.tsx
+++ apps/bridge/components/bridge-header.tsx
@@ -1,14 +1,25 @@
+import { useTransactions } from "@/hooks/use-transactions";
 import { useConfigState } from "@/state/config";
 
 export const BridgeHeader = () => {
   const setDisplayTransactions = useConfigState.useSetDisplayTransactions();
 
+  const { inProgressCount } = useTransactions();
+
   return (
     <>
-      <div className="flex items-center gap-2 px-4 md:px-6 pt-3 md:pt-6 pb-2 md:pb-4">
+      <div className="flex items-center gap-4 px-4 md:px-6 pt-3 md:pt-6 pb-2 md:pb-4">
         <span>Bridge</span>
-        <button onClick={() => setDisplayTransactions(true)}>Activity</button>
 
+        <button
+          className="flex items-center gap-1"
+          onClick={() => setDisplayTransactions(true)}
+        >
+          <span>Activity</span>
+          <span className="rounded-full bg-gray-100 p-1">
+            {inProgressCount}
+          </span>
+        </button>
         <span className="ml-auto">Settings</span>
       </div>
     </>
