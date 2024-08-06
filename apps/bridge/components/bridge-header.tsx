129896f1ff40b4519b0bfa612aa2a64c0b9d43e1
James Stackhouse <james@houseofstack.com>
2024-08-06 13:50:38 +1000

    Better activity header

--- apps/bridge/components/bridge-header.tsx
+++ apps/bridge/components/bridge-header.tsx
@@ -1,8 +1,10 @@
+import clsx from "clsx";
+
 import { ModalNames } from "@/constants/modal-names";
 import { useTransactions } from "@/hooks/use-transactions";
 import { useConfigState } from "@/state/config";
 
-import { IconSettings, IconSpinner } from "./icons";
+import { IconActivity, IconSettings, IconSpinner } from "./icons";
 
 export const BridgeHeader = () => {
   const setDisplayTransactions = useConfigState.useSetDisplayTransactions();
@@ -16,28 +18,37 @@ export const BridgeHeader = () => {
         <h2 className="font-heading leading-none text-sm px-2 py-1.5">
           Bridge
         </h2>
-        <div className="flex gap-2 items-center">
+        <div className="flex gap-1 items-center">
           <button
-            className="group flex items-center gap-2 text-foreground rounded-full transition-all hover:bg-muted rounded-full py-1.5 pl-2 pr-2.5"
+            className={clsx(
+              inProgressCount > 0 ? "bg-muted pl-2.5" : "bg-transparent",
+              "group flex items-center gap-2 text-foreground rounded-full transition-all rounded-full py-1  px-1"
+            )}
             onClick={() => setDisplayTransactions(true)}
           >
             {/* TODO: Put "action required" text here as needed */}
             {inProgressCount > 0 && (
-              <div className="flex items-center gap-1 text-primary-foreground bg-primary rounded-full px-1.5 py-1">
-                <IconSpinner className="w-3 h-3 shrink-0" />
-                <span className="text-sm font-heading leading-none">
-                  {inProgressCount}
+              <div className="flex items-center gap-1">
+                <span className="text-xs font-body leading-none">
+                  {inProgressCount} Active
                 </span>
               </div>
             )}
-            <span className="font-heading text-sm leading-none">Activity</span>
-            {/* <IconActivity className="fill-muted-foreground group-hover:fill-foreground group-hover:animate-wiggle-waggle transition-all h-2.5 w-auto shrink-0" /> */}
+            {/* <span className="font-heading text-sm leading-none">Activity</span> */}
+            <IconActivity
+              className={clsx(
+                inProgressCount > 0
+                  ? "fill-foreground animate-wiggle-waggle"
+                  : "fill-muted-foreground",
+                "group-hover:fill-foreground group-hover:animate-wiggle-waggle transition-all h-5 w-5 shrink-0"
+              )}
+            />
           </button>
           <button
-            className="group flex items-center justify-center"
+            className="group flex items-center justify-center p-1 rounded-full"
             onClick={() => addModal(ModalNames.Settings)}
           >
-            <IconSettings className="fill-muted-foreground group-hover:fill-foreground transition-all group-hover:rotate-45 group-hover:scale-105 h-5 w-5" />
+            <IconSettings className="fill-muted-foreground group-hover:fill-foreground transition-all group-hover:rotate-45 group-hover:scale-105 h-5 w-5 shrink-0" />
           </button>
         </div>
       </div>
