96c7afa24cc64e65ae34acd37171aa2f6ad2d0b9
James Stackhouse <james@houseofstack.com>
2024-08-09 15:02:25 +1000

    percentage based corners on network icons

--- apps/bridge/components/network-icon.tsx
+++ apps/bridge/components/network-icon.tsx
@@ -47,7 +47,7 @@ export const NetworkIcon = ({
     <img
       key={`${chain?.name}-network-icon`}
       {...props}
-      className={clsx(props.className, "rounded-full")}
+      className={clsx(props.className, "rounded-[23%]")}
       alt={`${chain?.name}-network-icon`}
       src={src}
     />
