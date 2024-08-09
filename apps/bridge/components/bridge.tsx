85c8b3198b80f0a9f221c6dfea7a4f056898aabd
James Stackhouse <james@houseofstack.com>
2024-08-09 15:02:49 +1000

    Attempted height transitions

--- apps/bridge/components/bridge.tsx
+++ apps/bridge/components/bridge.tsx
@@ -1,5 +1,6 @@
 import { useDeployment } from "@/hooks/deployments/use-deployment";
 
+import { AnimateChangeInHeight } from "./animate-height";
 import { Banners } from "./banners";
 import { BridgeBadges } from "./bridge-badges";
 import { BridgeBody } from "./bridge-body";
@@ -14,25 +15,27 @@ export const Bridge = () => {
       className="flex items-start justify-center w-screen h-screen fixed inset-0 overflow-y-auto overflow-x-hidden"
       key="bridgeMain"
     >
-      <div className="w-full px-2 md:px-0  md:w-[420px] aspect-[3/4] relative mb-24 mt-28 md:mt-24 2xl:mt-32">
-        <div className="flex flex-col gap-2 items-center">
-          <Banners />
-          <div
-            className={`bg-card mx-auto rounded-[24px] md:rounded-[32px] shadow-sm w-full shrink-0 backdrop-blur-sm`}
-          >
-            {deployment?.deletedAt &&
-            new Date(deployment.deletedAt) < new Date() ? (
-              <BridgeDeleted />
-            ) : (
-              <>
-                <BridgeHeader />
-                <BridgeBody />
-                <BridgeBadges />
-              </>
-            )}
+      <AnimateChangeInHeight>
+        <div className="w-full px-2 md:px-0  md:w-[468px] relative mb-24 mt-28 md:mt-24 2xl:mt-32">
+          <div className="flex flex-col gap-2 items-center">
+            <Banners />
+            <div
+              className={`bg-card mx-auto rounded-[24px] md:rounded-[32px] shadow-sm w-full shrink-0 backdrop-blur-sm`}
+            >
+              {deployment?.deletedAt &&
+              new Date(deployment.deletedAt) < new Date() ? (
+                <BridgeDeleted />
+              ) : (
+                <>
+                  <BridgeHeader />
+                  <BridgeBody />
+                  <BridgeBadges />
+                </>
+              )}
+            </div>
           </div>
         </div>
-      </div>
+      </AnimateChangeInHeight>
     </main>
   );
 };
