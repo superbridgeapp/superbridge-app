6c97f84259dc063bc1c3f8d057e70bb326fb32b3
Alex Harley <alex@alexbh.dev>
2024-08-12 11:42:24 +0200

    chore: v3.superbridge.app

--- apps/bridge/constants/hosts.ts
+++ apps/bridge/constants/hosts.ts
@@ -1,2 +1,3 @@
 export const SUPERBRIDGE_HOST = "superbridge.app";
 export const SUPERBRIDGE_TESTNET_HOST = "testnets.superbridge.app";
+export const V3_SUPERBRIDGE_HOST = "v3.superbridge.app";
--- apps/bridge/utils/is-superbridge.ts
+++ apps/bridge/utils/is-superbridge.ts
@@ -1,4 +1,10 @@
-import { SUPERBRIDGE_HOST, SUPERBRIDGE_TESTNET_HOST } from "@/constants/hosts";
+import {
+  SUPERBRIDGE_HOST,
+  SUPERBRIDGE_TESTNET_HOST,
+  V3_SUPERBRIDGE_HOST,
+} from "@/constants/hosts";
 
 export const isSuperbridge = (host: string) =>
-  host === SUPERBRIDGE_HOST || host === SUPERBRIDGE_TESTNET_HOST;
+  host === SUPERBRIDGE_HOST ||
+  host === SUPERBRIDGE_TESTNET_HOST ||
+  host === V3_SUPERBRIDGE_HOST;
