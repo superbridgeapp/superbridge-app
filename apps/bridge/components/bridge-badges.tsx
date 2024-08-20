4c82186328ab38cb6a643aaad199193b1562b8c1
Alex Harley <alex@alexbh.dev>
2024-08-20 09:11:27 +0200

    fix: powered by badges on rollies

--- apps/bridge/components/bridge-badges.tsx
+++ apps/bridge/components/bridge-badges.tsx
@@ -2,36 +2,47 @@ import { isPresent } from "ts-is-present";
 
 import { DeploymentType } from "@/codegen/model";
 import { useIsSuperbridge } from "@/hooks/apps/use-is-superbridge";
-import { useDeployment } from "@/hooks/deployments/use-deployment";
 import { useDeployments } from "@/hooks/deployments/use-deployments";
 import { useInjectedStore } from "@/state/injected";
 
+import { PoweredByAlchemy } from "./badges/powered-by-alchemy";
+import { PoweredByAltLayer } from "./badges/powered-by-alt-layer-badge";
 import { PoweredByConduit } from "./badges/powered-by-conduit-badge";
 import { TestnetBadge } from "./badges/testnet-badge";
 
 export const BridgeBadges = () => {
-  const deployment = useDeployment();
   const deployments = useDeployments();
   const superbridgeTestnets = useInjectedStore((s) => s.superbridgeTestnets);
   const isSuperbridge = useIsSuperbridge();
 
   const testnetBadge =
     (isSuperbridge && superbridgeTestnets) ||
-    (!isSuperbridge && deployments[0]?.type === DeploymentType.testnet) ? (
+    (deployments.length === 1 &&
+      deployments[0]?.type === DeploymentType.testnet) ? (
       <TestnetBadge />
     ) : null;
 
   const poweredByConduit =
-    deployment?.provider === "conduit" && !isSuperbridge ? (
+    deployments.length === 1 && deployments[0]?.provider === "conduit" ? (
       <PoweredByConduit />
     ) : null;
 
   const poweredByAltLayer =
-    deployment?.provider === "alt-layer" && !isSuperbridge ? (
-      <PoweredByConduit />
+    deployments.length === 1 && deployments[0]?.provider === "alt-layer" ? (
+      <PoweredByAltLayer />
     ) : null;
 
-  const badges = [poweredByConduit, poweredByAltLayer].filter(isPresent);
+  const poweredByAlchemy =
+    deployments.length === 1 && deployments[0]?.name === "shape-testnet" ? (
+      <PoweredByAlchemy />
+    ) : null;
+
+  const badges = [
+    testnetBadge,
+    poweredByConduit,
+    poweredByAltLayer,
+    poweredByAlchemy,
+  ].filter(isPresent);
 
   if (!badges.length) {
     return null;
