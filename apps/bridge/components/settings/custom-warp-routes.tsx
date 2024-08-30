c3203ce967a8c5d1c4b9b1d3e02b59ddf9775d4e
Alex Harley <alex@alexbh.dev>
2024-08-30 11:49:09 +0200

    chore: build

--- apps/bridge/components/settings/custom-warp-routes.tsx
+++ apps/bridge/components/settings/custom-warp-routes.tsx
@@ -29,7 +29,7 @@ export const CustomWarpRoutes = () => {
         </div>
 
         <Button onClick={() => modal.open()} size={"xs"}>
-          Cutsomize
+          Customize
         </Button>
       </div>
     </div>
