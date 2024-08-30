18731be2e886944b9193c508e202fbfb7657d50e
James Stackhouse <james@houseofstack.com>
2024-08-30 18:33:21 +1000

    Custom Routes style

--- apps/bridge/components/modals/custom-warp-routes-modal.tsx
+++ apps/bridge/components/modals/custom-warp-routes-modal.tsx
@@ -1,11 +1,19 @@
+import { DialogTitle } from "@radix-ui/react-dialog";
 import { useEffect, useState } from "react";
 
 import { useHyperlaneControllerResolveWarpRouteYamlFile } from "@/codegen/index";
+import { Label } from "@/components/ui/label";
+import { Textarea } from "@/components/ui/textarea";
 import { useModal } from "@/hooks/use-modal";
 import { useHyperlaneState } from "@/state/hyperlane";
 
 import { Button } from "../ui/button";
-import { Dialog, DialogContent } from "../ui/dialog";
+import {
+  Dialog,
+  DialogContent,
+  DialogFooter,
+  DialogHeader,
+} from "../ui/dialog";
 
 export const CustomWarpRoutesModal = () => {
   const modal = useModal("CustomWarpRoutes");
@@ -39,17 +47,26 @@ export const CustomWarpRoutesModal = () => {
   return (
     <Dialog open={modal.isOpen} onOpenChange={modal.close}>
       <DialogContent>
-        <div className="flex flex-col gap-8 p-6">
-          <textarea
-            className="text-black p-2"
-            value={data}
-            onChange={(e) => setData(e.target.value)}
-          />
-
+        <DialogHeader>
+          <DialogTitle>Custom Warp Routes</DialogTitle>
+        </DialogHeader>
+        <div className="flex flex-col px-6">
+          <div className="grid w-full gap-2">
+            <Label htmlFor="warproutes">YAML</Label>
+            <Textarea
+              id="warproutes"
+              value={data}
+              className="min-h-56 text-xs p-4"
+              onChange={(e) => setData(e.target.value)}
+              placeholder="Paste your YAML Hyperlane routes"
+            />
+          </div>
+        </div>
+        <DialogFooter>
           <Button disabled={resolveWarpRoutes.isPending} onClick={onSave}>
             Save
           </Button>
-        </div>
+        </DialogFooter>
       </DialogContent>
     </Dialog>
   );
---
+++ apps/bridge/components/ui/textarea.tsx
@@ -0,0 +1,24 @@
+import * as React from "react";
+
+import { cn } from "@/utils";
+
+export interface TextareaProps
+  extends React.TextareaHTMLAttributes<HTMLTextAreaElement> {}
+
+const Textarea = React.forwardRef<HTMLTextAreaElement, TextareaProps>(
+  ({ className, ...props }, ref) => {
+    return (
+      <textarea
+        className={cn(
+          "flex min-h-[80px] w-full rounded-md border bg-muted px-3 py-2 text-sm ring-offset-card placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-0 focus-visible:ring-border focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50",
+          className
+        )}
+        ref={ref}
+        {...props}
+      />
+    );
+  }
+);
+Textarea.displayName = "Textarea";
+
+export { Textarea };
