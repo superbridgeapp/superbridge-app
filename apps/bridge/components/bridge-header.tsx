96bad9d8de991f1fa986e2fc9166d71ac0189a3d
James Stackhouse <james@houseofstack.com>
2024-09-06 18:43:32 +1000

    Widget utility menu

--- apps/bridge/components/bridge-header.tsx
+++ apps/bridge/components/bridge-header.tsx
@@ -1,6 +1,14 @@
 import clsx from "clsx";
+import Link from "next/link";
+import { useAccount } from "wagmi";
 
 import { DeploymentType } from "@/codegen/model";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
 import { useIsSpecialApp } from "@/hooks/apps/use-is-special-app";
 import { useIsSuperbridge } from "@/hooks/apps/use-is-superbridge";
 import { useDeployments } from "@/hooks/deployments/use-deployments";
@@ -11,7 +19,14 @@ import { useConfigState } from "@/state/config";
 import { useInjectedStore } from "@/state/injected";
 
 import { TestnetBadge } from "./badges/testnet-badge";
-import { IconActivity, IconSettings, IconSpinner } from "./icons";
+import {
+  IconActivity,
+  IconArrowUpRight,
+  IconEllip,
+  IconSB,
+  IconSettings,
+  IconSpinner,
+} from "./icons";
 
 export const BridgeHeader = () => {
   const setDisplayTransactions = useConfigState.useSetDisplayTransactions();
@@ -25,6 +40,7 @@ export const BridgeHeader = () => {
   const isSpecialApp = useIsSpecialApp();
 
   const isWidget = useIsWidget();
+  const account = useAccount();
 
   return (
     <>
@@ -42,6 +58,15 @@ export const BridgeHeader = () => {
             <TestnetBadge />
           </div>
         ) : null}
+
+        {/* {account.address && isWidget && (
+          <div className="pl-0.5 mr-auto">
+            <span className="bg-muted rounded-full text-xs text-muted-foreground px-3 py-1 h-6">
+              {account.address.slice(0, 4)}&hellip;
+              {account.address.slice(account.address.length - 4)}
+            </span>
+          </div>
+        )} */}
 
         <div className="flex gap-1.5 items-center">
           <button
@@ -78,6 +103,53 @@ export const BridgeHeader = () => {
           >
             <IconSettings className="fill-muted-foreground group-hover:fill-foreground transition-all group-hover:rotate-[15deg] group-hover:scale-105 h-5 w-5 shrink-0" />
           </button>
+
+          {isWidget && (
+            <DropdownMenu>
+              <DropdownMenuTrigger className="bg-muted group hover:scale-105 transition-all flex items-center justify-center py-1.5 px-2 rounded-full outline-none focus:outline-none focus-visible:ring-transparent focus:ring-transparent ring-transparent">
+                <IconEllip className="fill-muted-foreground group-hover:fill-foreground transition-all group-hover:scale-105 h-5 w-5 shrink-0" />
+              </DropdownMenuTrigger>
+              <DropdownMenuContent>
+                {account.address ? (
+                  <DropdownMenuItem className="flex justify-between items-center w-full">
+                    <span className="text-sm">Disconnect</span>
+                    <span className="bg-muted rounded-full text-xs text-muted-foreground px-2 py-1 h-6">
+                      {account.address.slice(0, 4)}&hellip;
+                      {account.address.slice(account.address.length - 4)}
+                    </span>
+                  </DropdownMenuItem>
+                ) : (
+                  <DropdownMenuItem>
+                    <span className="text-sm">Connect</span>
+                  </DropdownMenuItem>
+                )}
+
+                <div className="bg-muted p-3 rounded-lg flex flex-col gap-3 mt-3 z-50">
+                  <a
+                    href="https://superbridge.app"
+                    className=" text-xs  leading-none w-full flex gap-2 items-center"
+                  >
+                    <IconSB className="h-4 w-auto fill-foreground" />
+                    <span>Powered by Superbridge</span>
+                  </a>
+                  <Link
+                    href="https://help.superbridge.app"
+                    className=" text-xs  leading-none w-full flex gap-2 items-center"
+                  >
+                    <IconArrowUpRight className="mx-0.5 h-3 w-3 fill-muted-foreground" />
+                    <span>Support & FAQs</span>
+                  </Link>
+                  <button
+                    className=" text-xs  leading-none w-full flex gap-2 items-center"
+                    // onClick={() => legalModal.open()}
+                  >
+                    <IconArrowUpRight className="mx-0.5 h-3 w-3 w-auto fill-muted-foreground" />
+                    <span>Legal</span>
+                  </button>
+                </div>
+              </DropdownMenuContent>
+            </DropdownMenu>
+          )}
         </div>
       </div>
     </>
