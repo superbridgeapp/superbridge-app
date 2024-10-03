685141f2031efdedc0c1994f314c313c72ebad43
Alex Harley <alex@alexbh.dev>
2024-10-03 12:48:35 +0200

    chore: capitalise

--- apps/bridge/hooks/providers/use-provider-name.ts
+++ apps/bridge/hooks/providers/use-provider-name.ts
@@ -5,6 +5,6 @@ export const useProviderName = (provider: RouteProvider | null) => {
   if (!provider) return "";
 
   if (provider === RouteProvider.Lz) return "Layer Zero";
-  if (nativeRoutes.includes(provider)) return "Native bridge";
+  if (nativeRoutes.includes(provider)) return "Native Bridge";
   return provider.toString();
 };
