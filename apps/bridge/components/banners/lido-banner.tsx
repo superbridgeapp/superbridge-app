ad714cf620af62c671899cb78031d3be8f01dc8a
Alex Harley <alex@alexbh.dev>
2024-10-07 13:21:02 +0200

    chore: lido banner

--- apps/bridge/components/banners/lido-banner.tsx
+++ apps/bridge/components/banners/lido-banner.tsx
@@ -3,31 +3,18 @@ import { Alert, AlertDescription, AlertTitle } from "../ui/alert";
 export const LidoBanner = () => {
   return (
     <Alert>
-      <AlertTitle>Please note</AlertTitle>
+      <AlertTitle>wstETH Deposits Paused</AlertTitle>
       <AlertDescription>
-        From 7th of October the availability for wstETH Ethereum to Optimism
-        bridge deposits will be paused.{" "}
+        Please note that wstETH Ethereum to OP Mainnet bridge deposits are
+        paused and unavailable{" "}
         <a
-          href="https://snapshot.org/#/lido-snapshot.eth/proposal/0xb1a3c33a4911712770c351504bac0499611ceb0faff248eacb1e96354f8e21e8"
+          href="https://research.lido.fi/t/lip-22-steth-on-l2/6855"
           target="_blank"
           className="underline"
         >
           Learn more
         </a>
       </AlertDescription>
-      {/* After 7th Oct */}
-      {/* <AlertTitle>wstETH Deposits Paused</AlertTitle> */}
-      {/* <AlertDescription>
-        Please note that wstETH Ethereum to Optimism bridge deposits are paused
-        and unavailable{" "}
-        <a
-          href="https://lido.fi/lido-multichain"
-          target="_blank"
-          className="underline"
-        >
-          Learn more
-        </a>
-      </AlertDescription> */}
     </Alert>
   );
 };
