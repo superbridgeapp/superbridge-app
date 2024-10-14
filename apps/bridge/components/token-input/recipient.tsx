32f42be4c5fd48bcf058067e1e20509ef0f0af14
Alex Harley <alex@alexbh.dev>
2024-10-14 14:28:49 +0200

    fix: recipient modal clean up

--- apps/bridge/components/token-input/recipient.tsx
+++ apps/bridge/components/token-input/recipient.tsx
@@ -1,10 +1,11 @@
 import clsx from "clsx";
+import { isAddressEqual } from "viem";
 import { useAccount } from "wagmi";
 
 import { useModal } from "@/hooks/use-modal";
 import { useConfigState } from "@/state/config";
 
-import { IconAddCircle, IconWallet } from "../icons";
+import { IconWallet } from "../icons";
 
 export const Recipient = () => {
   const recipientName = useConfigState.useRecipientName();
@@ -19,21 +20,17 @@ export const Recipient = () => {
     >
       {!account.address ? (
         <span className={"text-xs text-muted-foreground"}>{/* empty */}</span>
-      ) : !recipientAddress ? (
-        <div className="flex justify-center items-center gap-1 pl-2 pr-1 py-1 rounded-full cursor-pointer hover:scale-105 transition-all bg-card">
-          <span className="text-[10px] font-button leading-none">
-            Add address
-          </span>
-          <IconAddCircle className="w-3 h-3 fill-foreground" />
-        </div>
       ) : (
         <div
           className={clsx(
             `h-5 flex justify-center items-center gap-1 px-2 py-1 mr-0.5 rounded-full cursor-pointer hover:scale-105 transition-all bg-card`,
-            account.address !== recipientAddress && "bg-card text-foreground"
+            recipientAddress &&
+              !isAddressEqual(account.address, recipientAddress) &&
+              "bg-card text-foreground"
           )}
         >
-          {account.address !== recipientAddress ? (
+          {recipientAddress &&
+          !isAddressEqual(account.address, recipientAddress) ? (
             <>
               <span className="text-[10px] font-button leading-none">
                 {recipientName
